8bc1418d173491579bf31562619449229adf42fb
Sovahless <stazsama@protonmail.com>
2022-05-03 15:47:12 +0200

    Selection des Film

--- main.py
+++ main.py
@@ -1,14 +1,59 @@
 import wx
 import main_form
+from switchcase import switch
 
+currentmovie = None
 
-class MainFrame(main_form.MyFrame3):
+class MainFrame(main_form.MyFrame1):
 
     def __init__(self, parent, title):
-        main_form.MyFrame3.__init__(self, parent)
+        main_form.MyFrame1.__init__(self, parent)
 
     def fermer(self, event):
         print(event)
+
+    def imgchange(self, event):
+        imgselection = self.m_comboBox2.GetSelection()
+
+        for case in switch(imgselection):
+            if case(0):
+                self.m_bitmap4.SetBitmap(wx.Bitmap('img/avengers.bmp'))
+                currentmovie = "Avengers"
+            if case(1):
+                self.m_bitmap4.SetBitmap(wx.Bitmap('img/chihiro.bmp'))
+                currentmovie = "Chihiro"
+            if case(2):
+                self.m_bitmap4.SetBitmap(wx.Bitmap('img/spiderman.bmp'))
+                currentmovie = "Spiderman No Way Home"
+            if case(3):
+                self.m_bitmap4.SetBitmap(wx.Bitmap('img/batman.bmp'))
+                currentmovie = "The Batman"
+            if case(4):
+                self.m_bitmap4.SetBitmap(wx.Bitmap('img/collini.bmp'))
+                currentmovie = "L'Affaire Collini"
+            if case(5):
+                self.m_bitmap4.SetBitmap(wx.Bitmap('img/morbius.bmp'))
+                currentmovie = "Morbius"
+            if case(6):
+                self.m_bitmap4.SetBitmap(wx.Bitmap('img/ogre.bmp'))
+                currentmovie = "Ogre"
+            if case(7):
+                self.m_bitmap4.SetBitmap(wx.Bitmap('img/abuela.bmp'))
+                currentmovie = "Abuela"
+            if case(8):
+                self.m_bitmap4.SetBitmap(wx.Bitmap('img/sonic.bmp'))
+                currentmovie = "Sonic 2"
+            if case(9):
+                self.m_bitmap4.SetBitmap(wx.Bitmap('img/secret.bmp'))
+                currentmovie = "Le secret de la cité perdu"
+            if case(10):
+                self.m_bitmap4.SetBitmap(wx.Bitmap('img/jujutsu.bmp'))
+                currentmovie = "Jujutsu Kaisen 0"
+            if case(11):
+                self.m_bitmap4.SetBitmap(wx.Bitmap('img/barbapapa.bmp'))
+                currentmovie = "Les Barbapapa"
+
+        print(imgselection)
 
 
 class MainApp(wx.App):
--- main_form.py
+++ main_form.py
@@ -17,23 +17,23 @@ import wx.xrc
 class MyFrame1 ( wx.Frame ):
 
 	def __init__( self, parent ):
-		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 500,300 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 771,688 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
 
 		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
 
 		gSizer1 = wx.GridSizer( 0, 1, 0, 0 )
 
-		self.m_staticText1 = wx.StaticText( self, wx.ID_ANY, u"Cinéma CGR", wx.DefaultPosition, wx.DefaultSize, 0 )
-		self.m_staticText1.Wrap( -1 )
+		self.m_staticText2 = wx.StaticText( self, wx.ID_ANY, u"Cinéma CGR", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText2.Wrap( -1 )
 
-		gSizer1.Add( self.m_staticText1, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
+		gSizer1.Add( self.m_staticText2, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
 
-		self.m_bitmap2 = wx.StaticBitmap( self, wx.ID_ANY, wx.NullBitmap, wx.DefaultPosition, wx.DefaultSize, 0 )
-		gSizer1.Add( self.m_bitmap2, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
+		self.m_bitmap4 = wx.StaticBitmap( self, wx.ID_ANY, wx.Bitmap( u"../../PycharmProjects/pythonProject1/img/avengers.bmp", wx.BITMAP_TYPE_ANY ), wx.DefaultPosition, wx.DefaultSize, 0 )
+		gSizer1.Add( self.m_bitmap4, 0, wx.ALIGN_CENTER_HORIZONTAL|wx.ALL, 5 )
 
 		m_comboBox2Choices = [ u"Avengers Endame", u"Voyage de Chihiro", u"Spiderman No Way Home", u"The Batman", u"L'affaire Collini", u"Morbius", u"Ogre", u"Abuela", u"Sonic 2", u"Le secret de la cité perdu", u"Jujutsu Kaisen 0", u"Les Barbapapa" ]
 		self.m_comboBox2 = wx.ComboBox( self, wx.ID_ANY, u"Choissisez un Film", wx.DefaultPosition, wx.DefaultSize, m_comboBox2Choices, 0 )
-		gSizer1.Add( self.m_comboBox2, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
+		gSizer1.Add( self.m_comboBox2, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL|wx.ALIGN_BOTTOM, 5 )
 
 		self.m_button2 = wx.Button( self, wx.ID_ANY, u"Choisir ce Film", wx.DefaultPosition, wx.DefaultSize, 0 )
 		gSizer1.Add( self.m_button2, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
@@ -44,8 +44,16 @@ class MyFrame1 ( wx.Frame ):
 
 		self.Centre( wx.BOTH )
 
+		# Connect Events
+		self.m_comboBox2.Bind( wx.EVT_COMBOBOX, self.imgchange )
+
 	def __del__( self ):
 		pass
+
+
+	# Virtual event handlers, override them in your derived class
+	def imgchange( self, event ):
+		event.Skip()
 
 
 ###########################################################################
